eca61c243981c48e362e3b953ec04044591ef5c7
Surabhi Seetharam <sseetha2@ncsu.edu>
2013-04-26 22:13:41 -0400

    Revert "Adding code for uploading SMS logs"
    This reverts commit 8a68bd5774cbbbc808ccdd6c0251cb8dd3a82a0e.

--- CallLog/src/com/example/calllog/MainActivity.java
+++ CallLog/src/com/example/calllog/MainActivity.java
@@ -142,11 +142,9 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CallLog;
 import android.telephony.TelephonyManager;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
@@ -161,14 +159,6 @@ public class MainActivity extends Activity {
 
 	public String myPhoneNumber= null;
 	ParseObject testObject = null;
-	ParseObject SMSObject = null;
-	TextView content;
-	StringBuffer sb;
-	
-	public MainActivity(){
-		
-		sb = new StringBuffer();
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -176,12 +166,8 @@ public class MainActivity extends Activity {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		content = (TextView) findViewById(R.id.text);
-		content.setMovementMethod(new ScrollingMovementMethod());
 		Parse.initialize(this, "pPowuNIyQqkuCJaexMKnhAlbzDbTa1mi2qRqjSsk", "o3d6OdaXbwbHpenQvKUSu7E5NrtsWMZuD6cpkPvc");
 		getCallDetails();
-		inSms();
-	    outSms();
 
 	}
 
@@ -366,139 +352,5 @@ public class MainActivity extends Activity {
 		        }
 		    }
 		});
-	}//getCallDetails
-	
-	@SuppressLint("SdCardPath")
-	private void inSms() {
-		
-		String FILENAME = "inSMS";
-		Integer inPosition = -1;
-		File directory = new File("/sdcard/SMSDetails/");
-		// have the object build the directory structure, if needed.
-		directory.mkdirs();		
-		File file = new File(directory,FILENAME);
-		boolean newFile = true;
-		
-		String address=null,date=null,body=null;
-		
-		try {
-
-			FileInputStream fis = new FileInputStream(file);
-			InputStreamReader inputStreamReader = new InputStreamReader(fis);
-			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-			String str = bufferedReader.readLine(); 
-			inPosition = Integer.parseInt(str);
-			Log.d("SMSlogsValue", "Current value in inSMS file is: " + inPosition);
-			fis.close();
-			newFile = false;
-
-		} catch (Exception e) {
-			Log.d("SMSfileExists", "Error opening file. File doesn't exist or some other error. Here is the stackTrace: \n" + e.toString() + "\nWill try to create a new file");
-			e.printStackTrace();
-		}
-		
-	    Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
-	    if (!newFile)
-			cursor.moveToPosition(inPosition);
-
-		    while(cursor.moveToNext()){
-		    
-		      inPosition = cursor.getPosition();
-		      
-		      address = cursor.getString(cursor.getColumnIndex("address"));
-		      date = cursor.getString(cursor.getColumnIndex("date"));
-		      body = cursor.getString(cursor.getColumnIndex("body"));
-		      
-		      SMSObject = new ParseObject("SMSObject");
-		     
-				SMSObject.put("Destination", myPhoneNumber);
-				SMSObject.put("Source", address);
-				SMSObject.put("date", date);
-				SMSObject.put("body", body);
-				SMSObject.saveInBackground();
-		      
-		    }//while
-		    
-		    
-		    try {
-
-				FileOutputStream fos = new FileOutputStream(file);
-				byte[] buffer = inPosition.toString().getBytes();
-				fos.write(buffer);
-				fos.close();			
-				Log.d("fileWrite", "Writing inbox position: " + inPosition + " succeeded. :)");
-
-			} catch (Exception e) {
-				Log.d("fileWrite", "Writing inbox position: " + inPosition + " failed!");
-				e.printStackTrace();
-			}
-		    cursor.close();
-	    }//inSMS
-	    
-	 @SuppressLint("SdCardPath")
-	private void outSms() {
-		 
-		 	String FILENAME = "outSMS";
-			Integer outPosition = -1;
-			File directory = new File("/sdcard/SMSDetails/");
-			// have the object build the directory structure, if needed.
-			directory.mkdirs();		
-			File file = new File(directory,FILENAME);
-			boolean newFile = true;
-			
-			String address=null,date=null,body=null;
-			
-			try {
-
-				FileInputStream fis = new FileInputStream(file);
-				InputStreamReader inputStreamReader = new InputStreamReader(fis);
-				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-				String str = bufferedReader.readLine(); 
-				outPosition = Integer.parseInt(str);
-				Log.d("SMSlogsValue", "Current value in outSMS file is: " + outPosition);
-				fis.close();
-				newFile = false;
-
-			} catch (Exception e) {
-				Log.d("SMSOUTfileExists", "Error opening file. File doesn't exist or some other error. Here is the stackTrace: \n" + e.toString() + "\nWill try to create a new file");
-				e.printStackTrace();
-			}
-			
-		    Cursor cursor = getContentResolver().query(Uri.parse("content://sms/sent"), null, null, null, null);
-		    if (!newFile)
-				cursor.moveToPosition(outPosition);
-
-			    while(cursor.moveToNext()){
-			    	
-			      outPosition = cursor.getPosition();
-			      
-			      address = cursor.getString(cursor.getColumnIndex("address"));
-			      date = cursor.getString(cursor.getColumnIndex("date"));
-			      body = cursor.getString(cursor.getColumnIndex("body"));
-			      
-			      SMSObject = new ParseObject("SMSObject");
-			     
-					SMSObject.put("Destination", address);
-					SMSObject.put("Source", myPhoneNumber);
-					SMSObject.put("date", date);
-					SMSObject.put("body", body);
-					SMSObject.saveInBackground();
-			      
-			    }//while
-			    
-			    
-			    try {
-
-					FileOutputStream fos = new FileOutputStream(file);
-					byte[] buffer = outPosition.toString().getBytes();
-					fos.write(buffer);
-					fos.close();			
-					Log.d("fileWrite", "Writing sent position: " + outPosition + " succeeded. :)");
-
-				} catch (Exception e) {
-					Log.d("fileWrite", "Writing sent position: " + outPosition + " failed!");
-					e.printStackTrace();
-				}
-			    cursor.close();
-	    }//outSMS
+	}
 }
